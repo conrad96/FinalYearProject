00595b4379f3d1277d3c508ca984ccda26f06e0c
conrad96 <conrad.mugisha@gmail.com>
2018-09-11 12:56:21 +0300

    Modified the login page

---
+++ application/views/index-tmp.php
@@ -0,0 +1,68 @@
+<?php include("header.php"); ?>
+<center><?php if(isset($msg))echo $msg  ?></center>
+	<div class="login" style="padding-left: 40%;padding-top: 5%;">
+<div class="col-lg-6" >
+
+
+       <section class="pull-right" style='border: 1px solid gray;border-radius:5px;' id="log">
+       <button class='btn btn-default' onclick="disp_stud();" title='Click Here to Login as Student'>Students</button>
+       <button class='btn btn-default pull-right' onclick="disp_staff();" title='Click Here to Login as Staff i.e Administrator or Lecturer'>Staff</button>
+         <hr />
+<form action='<?php echo $assets['base_url'].'online/login_student'; ?>' method="post" id='stud' >
+         <div class="form-group">
+<div class="input-group">
+  <div class="input-group-addon">Student Number</div>
+  <input type="text" id="username3" name="studentNo" class="form-control" placeholder="Students Number" required>
+  <div class="input-group-addon"><i class="fa fa-user"></i></div>
+</div>
+</div>  <div class="form-group">
+<div class="input-group">
+  <div class="input-group-addon">Password</div>
+  <input type="password" id="password3" name="password" class="form-control" placeholder="Password" required>
+  <div class="input-group-addon"><i class="fa fa-asterisk"></i></div>
+</div>
+</div>
+<div class="form-actions form-group">
+<button type="submit" class="btn btn-primary btn-sm btn-lg" id="login_btn">Login</button>
+</div>
+
+</form>
+
+
+
+            <form action='<?php echo $assets['base_url'].'online/login'; ?>' method="post" style='display: none;' id='staff'>
+
+<div class="form-group">
+<div class="input-group">
+  <div class="input-group-addon">Email</div>
+  <input type="email" id="email3" name="email" class="form-control" required>
+  <div class="input-group-addon"><i class="fa fa-envelope"></i></div>
+</div>
+</div>
+<div class="form-group">
+<div class="input-group">
+  <div class="input-group-addon">Password</div>
+  <input type="password" id="password3" name="password" class="form-control" required>
+  <div class="input-group-addon"><i class="fa fa-asterisk"></i></div>
+</div>
+</div>
+<div class="form-actions form-group">
+<button type="submit" class="btn btn-primary btn-sm btn-lg" id="login_btn">Login</button>
+</div>
+            </form>
+            </section>
+
+                    </div>
+
+
+       </div>
+<script type="text/javascript">
+  function disp_stud(){
+document.getElementById("stud").style.display='block';
+document.getElementById("staff").style.display='none';
+  }
+function disp_staff(){
+document.getElementById("stud").style.display='none';
+document.getElementById("staff").style.display='block';
+}
+</script>
--- application/views/index.php
+++ application/views/index.php
@@ -1,68 +1,104 @@
+
 <?php include("header.php"); ?>
+<!-- Include the above in your HEAD tag ---------->
 <center><?php if(isset($msg))echo $msg  ?></center>
-	<div class="login" style="padding-left: 40%;padding-top: 5%;">
-<div class="col-lg-6" >
-
-
-       <section class="pull-right" style='border: 1px solid gray;border-radius:5px;' id="log">
-       <button class='btn btn-default' onclick="disp_stud();" title='Click Here to Login as Student'>Students</button>
-       <button class='btn btn-default pull-right' onclick="disp_staff();" title='Click Here to Login as Staff i.e Administrator or Lecturer'>Staff</button>
-         <hr />
-<form action='<?php echo $assets['base_url'].'online/login_student'; ?>' method="post" id='stud' >
-         <div class="form-group">
-<div class="input-group">
-  <div class="input-group-addon">Student Number</div>
-  <input type="text" id="username3" name="studentNo" class="form-control" placeholder="Students Number" required>
-  <div class="input-group-addon"><i class="fa fa-user"></i></div>
+<!-- BEGIN # BOOTSNIP INFO -->
+<div class="container">
+	<div class="row" style="padding-top:100px;">
+    <span >
+		<h1 class="text-center">WELCOME</h1>
+    <p style="padding-top:40px;">
+      <blockquote>
+This is a Learning Platform  integrated set of interactive online services that provides the teachers, learners, parents and others involved in education with information, tools and resources to support and enhance educational delivery and assessment
+</blockquote>
+    </p>
+    <br />
+    Click Below to Login<br />
+        <p class="text-center"><a href="#" class="btn btn-primary btn-lg" data-backdrop='static' role="button" data-toggle="modal" data-target="#login-modal">Enter System</a></p>
+      </span>
+  </div>
 </div>
-</div>  <div class="form-group">
-<div class="input-group">
-  <div class="input-group-addon">Password</div>
-  <input type="password" id="password3" name="password" class="form-control" placeholder="Password" required>
-  <div class="input-group-addon"><i class="fa fa-asterisk"></i></div>
-</div>
-</div>
-<div class="form-actions form-group">
-<button type="submit" class="btn btn-primary btn-sm btn-lg" id="login_btn">Login</button>
-</div>
+<!-- END # BOOTSNIP INFO -->
 
-</form>
+<!-- BEGIN # MODAL LOGIN -->
+<div class="modal fade" id="login-modal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true" style="display: none;">
+    	<div class="modal-dialog">
+			<div class="modal-content">
+				<div class="modal-header" align="center">
+					<img class="img-circle" id="img_logo" src="<?php echo $assets['image'].'logo_ols.png'; ?>">
+					<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+						<span class="glyphicon glyphicon-remove" aria-hidden="true"></span>
+					</button>
+				</div>
 
+                <!-- Begin # DIV Form -->
+                <div id="div-forms">
 
+                    <!-- Begin # Login Form -->
+                    <form id="login-form" action='<?php echo $assets['base_url'].'online/login_student'; ?>' method="post">
+		                <div class="modal-body">
+				    		<div id="div-login-msg">
+                                <div id="icon-login-msg" class="glyphicon glyphicon-chevron-right"></div>
+                                <span id="text-login-msg">Type your Student Number  and password.</span>
+                            </div>
+				    		<input id="login_username" class="form-control" type="number" name="studentNo" placeholder="Student Number" required>
+				    		<input id="login_password" class="form-control" type="password" name="password" placeholder="Password" required>
+                            <div class="checkbox">
+                                <label>
+                                    <input type="checkbox" checked> Remember me
+                                </label>
+                            </div>
+        		    	</div>
+				        <div class="modal-footer">
+                            <div>
+                                <button type="submit" class="btn btn-primary btn-lg btn-block">Login</button>
+                            </div>
+				    	    <div>
 
-            <form action='<?php echo $assets['base_url'].'online/login'; ?>' method="post" style='display: none;' id='staff'>
+                                <button id="login_register_btn" type="button" class="btn btn-link" ><i class='glyphicon glyphicon-hand-right'></i>&nbsp;Staff Login</button>
+                  </div>
+				        </div>
+                    </form>
+                    <!-- End # Login Form -->
 
-<div class="form-group">
-<div class="input-group">
-  <div class="input-group-addon">Email</div>
-  <input type="email" id="email3" name="email" class="form-control" required>
-  <div class="input-group-addon"><i class="fa fa-envelope"></i></div>
-</div>
-</div>
-<div class="form-group">
-<div class="input-group">
-  <div class="input-group-addon">Password</div>
-  <input type="password" id="password3" name="password" class="form-control" required>
-  <div class="input-group-addon"><i class="fa fa-asterisk"></i></div>
-</div>
-</div>
-<div class="form-actions form-group">
-<button type="submit" class="btn btn-primary btn-sm btn-lg" id="login_btn">Login</button>
-</div>
-            </form>
-            </section>
-
-                    </div>
-
-
-       </div>
 <script type="text/javascript">
-  function disp_stud(){
-document.getElementById("stud").style.display='block';
-document.getElementById("staff").style.display='none';
-  }
-function disp_staff(){
-document.getElementById("stud").style.display='none';
-document.getElementById("staff").style.display='block';
-}
+  $(document).ready(function(){
+    $("#login_register_btn").click(function(){
+      $("#login-form").hide();
+      $("#register-form").show();
+    });
+    $("#register_login_btn").click(function (){
+      $("#login-form").show();
+      $("#register-form").hide();
+    });
+  });
 </script>
+
+                    <!-- Begin | Register Form -->
+                    <form id="register-form" style="display:none;" action='<?php echo $assets['base_url'].'online/login'; ?>' method="post">
+            		    <div class="modal-body">
+		    				<div id="div-register-msg">
+                                <div id="icon-register-msg" class="glyphicon glyphicon-chevron-right"></div>
+                                <span id="text-register-msg">Staff Login.</span>
+                </div>
+                            <input id="register_email" class="form-control" type="email"  name="email" placeholder="E-Mail" required>
+                            <input id="register_password" class="form-control" type="password" name="password" placeholder="Password" required>
+            			</div>
+		    		    <div class="modal-footer">
+                            <div>
+                                <button type="submit" class="btn btn-primary btn-lg btn-block">Register</button>
+                            </div>
+                            <div>
+                                <button id="register_login_btn" type="button" class="btn btn-link"><i class='glyphicon glyphicon-hand-right'></i>&nbsp;Student Log In</button>
+                            </div>
+		    		    </div>
+                    </form>
+                    <!-- End | Register Form -->
+
+                </div>
+                <!-- End # DIV Form -->
+
+			</div>
+		</div>
+	</div>
+    <!-- END # MODAL LOGIN -->
